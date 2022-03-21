aae9904d89138e7c4c9ecf7a496ed6f9d55f709e
Ayoub Abidi <mrayoubabidi@gmail.com>
2022-03-21 16:43:58 +0100

    ✏️ Typo

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -8,8 +8,8 @@ const ProductList = () => {
       <div className="pl-texts">
         <h1 className="pl-title">My latest personal projects</h1>
         <p className="pl-desc">
-          I believe in learning by doing. This is why i try to implement new technologies in my projects so i can know what looks like to use it outside the tutorials and documentations.
-          The majority of all these project's source codes are publicly available on my <a class="link-info" href="https://github.com/ayoub3bidi" target="_blank" rel="noopener noreferrer">GitHub</a> account (projects builded with others are not included here).
+          I believe in learning by doing. This is why i try to implement new technologies in my projects so i can know what it looks like to use it outside the tutorials and documentations.
+          All these project's source codes are publicly available on my <a href="https://github.com/ayoub3bidi" target="_blank" rel="noopener noreferrer">GitHub</a> account (projects builded with others are not included here).
         </p>
       </div>
       <div className="pl-list">
