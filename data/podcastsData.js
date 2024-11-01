afb968e71c07d951128d7450f8a6fd20ccdc8b6e
Ayoub Abidi <mrayoubabidi@gmail.com>
2024-11-01 12:05:07 +0100

    feat: add podcasts page

---
+++ data/podcastsData.js
@@ -0,0 +1,20 @@
+const podcastsData = [
+  {
+    title: 'Code Conversations',
+    description: `The podcast where artificial intelligence meets the world of software development!`,
+    image: '/static/images/code_conversations.jpg',
+    link: 'https://podcasters.spotify.com/pod/show/code-conversations',
+    details:
+      "Join our AI hosts as they engage in insightful dialogues about critical topics, theories, and innovations in the tech industry. Each episode features deep dives into seminal papers, emerging technologies, and best practices, all crafted from the perspective of seasoned developers. Whether you're a novice coder or an experienced engineer, Code Conversations aims to spark thoughtful discussions that challenge conventional wisdom and inspire continuous learning.",
+  },
+  {
+    title: "GitHub's Tunisian Community",
+    description: `A vibrant podcast by and for Tunisian developers and tech enthusiasts, diving into all things software development.`,
+    image: '/static/images/github_tunisian_community.jpg',
+    link: 'https://podcasters.spotify.com/pod/show/githubtunisia',
+    details:
+      "Discover the perspectives and experiences of Tunisia’s thriving tech community! Each episode features local developers, students, and industry professionals exploring topics from open-source contributions to the latest programming trends. Join us as we discuss tech challenges, share insights, and celebrate the talent and innovation emerging from Tunisia's tech scene. Perfect for listeners who want to stay connected to the pulse of software development in Tunisia and beyond.",
+  },
+]
+
+module.exports = podcastsData
